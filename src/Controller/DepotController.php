3159fc88bb5ee5a32cb0984e0ad3923c194b4ecf
tamaraba <tamara.ba14@gmail.com>
2019-08-09 08:31:23 +0000

    faire un depot pour un partenaire

---
+++ src/Controller/DepotController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Depot;
+use App\Form\DepotType;
+use App\Repository\DepotRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/depot")
+ */
+class DepotController extends AbstractController
+{
+    /**
+     * @Route("/", name="depot_index", methods={"GET"})
+     */
+    public function index(DepotRepository $depotRepository): Response
+    {
+        return $this->render('depot/index.html.twig', [
+            'depots' => $depotRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="depot_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $depot = new Depot();
+        $form = $this->createForm(DepotType::class, $depot);
+        $form->handleRequest($request);
+        $data=json_decode($request->getContent(),true);
+        $form->submit($data);
+
+            $depot ->setDate(new \Datetime());
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($depot);
+            $entityManager->flush();
+
+            $compte=$depot->getCompte();
+            $compte->setMontant($compte->getMontant()+$depot->getMontant());
+            $entityManager->persist($compte);
+            $entityManager->flush();
+
+
+            return new Response('le depot a été bien fait', Response::HTTP_CREATED);
+
+    }
+
+    /**
+     * @Route("/{id}", name="depot_show", methods={"GET"})
+     */
+    public function show(Depot $depot): Response
+    {
+        return $this->render('depot/show.html.twig', [
+            'depot' => $depot,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="depot_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Depot $depot): Response
+    {
+        $form = $this->createForm(Depot1Type::class, $depot);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('depot_index');
+        }
+
+        return $this->render('depot/edit.html.twig', [
+            'depot' => $depot,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="depot_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Depot $depot): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$depot->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($depot);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('depot_index');
+    }
+}
